c1a5d6b149d4d86e785dcbb47a94572fcfeef65e
Garima Jain <2018csdgarima6532@poornima.ac.in>
2021-05-20 13:43:25 +0530

    after editing get movie

--- Repository/MovieRepository.cs
+++ Repository/MovieRepository.cs
@@ -25,10 +25,9 @@ namespace Webgentle.BookStore.Repository
         {
             return new List<MovieModel>()
             {
-                new MovieModel(){Id = 1,Title ="Notebook",Director="Monica",Description ="Short description about movie"},
-                new MovieModel(){Id = 1,Title ="Marvel",Director="jane",Description="Short description about movie"},
-                new MovieModel(){Id = 1,Title ="Marvel",Director="jane",Description="Short description about movie"},
-                new MovieModel(){Id = 1,Title ="Wonder Woman",Director="harry",Description="Short description about movie"},
+                new MovieModel(){Id = 1,Title ="Notebook",Director="Monica",Description ="The Notebook is a 2004 American romantic drama film directed by Nick Cassavetes, written by Jeremy Leven and Jan Sardi.The film stars Ryan Gosling and Rachel McAdams as a young couple who fall in love in the 1940s. Their story is read from a notebook in the present day by an elderly man (played by James Garner), telling the tale to a fellow nursing home resident"},
+                new MovieModel(){Id = 2,Title ="Marvel",Director="Jane",Description="   The Marvel Cinematic Universe (MCU) is an American media franchise and shared universe centered on a series of superhero films, independently produced by Marvel Studios and based on characters that appear in American comic books published by Marvel Comics. The franchise includes comic books, short films, television series, and digital series."},
+                new MovieModel(){Id = 3,Title ="Wonder Woman",Director="Harry",Description="Wonder Woman is a superheroine appearing in American comic books published by DC Comics. ... In her homeland, the island nation of Themyscira, her official title is Princess Diana of Themyscira. When blending into the society outside of her homeland, she sometimes adopts her civilian identity Diana Prince."},
                 
 
 
